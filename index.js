cef7e8723e1681be7eeda7b2dbf8fd2bbafa7f4e
Akshat Chaturvedi <akshatchaturvedi17@gmail.com>
2020-02-22 02:15:10 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const exphbs  = require('express-handlebars');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;	// use web-hoster's PORT or use 5000
+
+
+// Set Handlebars Middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+const otherstuff = "hello there, this is other stuff";
+
+// Set Handlebar routes
+app.get('/', function (req, res) {
+    res.render('home', {
+    	stuff: "This is stuff..." + otherstuff
+    });
+});
+
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(PORT, () => console.log('Server Listening on port ' + PORT))
